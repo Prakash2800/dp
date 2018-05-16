d42cfdfc03fce2c95ad15bdce9b162b053e52ba4
Prakash2800 <prakashbhunjun@yahoo.com>
2018-05-16 08:39:45 +0400

    setup - routing - contact form

---
+++ src/app/evaluation/evaluation.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EvaluationComponent } from './evaluation.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [EvaluationComponent]
+})
+export class EvaluationModule { }
---
+++ src/app/questions/questions.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { QuestionsComponent } from './questions.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [QuestionsComponent]
+})
+export class QuestionsModule { }
