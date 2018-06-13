1e367ac692a2f3940bfc5fbb0dc5eceb2622151e
Prakash2800 <prakashbhunjun@yahoo.com>
2018-06-13 07:15:07 +0400

    Eavaluation Logic

--- src/app/evaluation/evaluation.component.ts
+++ src/app/evaluation/evaluation.component.ts
@@ -8,23 +8,27 @@ import {BehaviorSubject} from 'rxjs/BehaviorSubject';
   styleUrls: ['./evaluation.component.scss']
 })
 export class EvaluationComponent implements OnInit {
+  question: string;
   op1: string;
   op2: string;
+  op3: string;
+  op4: string;
   index = 0;
-  index2 = 0;
-  public index2$: BehaviorSubject<number>;
+  disableButtonPrevious = true;
+  disableButtonNext = false;
+  questionsLength: number;
+
+  public index$: BehaviorSubject<number>;
 
   public questions = [];
   public questions2 = [
-      {id: 1, question: 'aaa', option1: 'q1o1', option2: 'q1o2', option3: 'q1o3', option4: 'q4o4'},
-      {id: 2, question: 'bbb', option1: 'q2o1', option2: 'q2o2', option3: 'q2o3', option4: 'q2o4'},
-      {id: 3, question: 'ccc', option1: 'q3o1', option2: 'q3o2', option3: 'q3o3', option4: 'q3o4'},
-      {id: 4, question: 'ddd', option1: 'q4o1', option2: 'q4o2', option3: 'q4o3', option4: 'q4o4'},
-      {id: 5, question: 'eee', option1: 'q5o1', option2: 'q5o2', option3: 'q5o3', option4: 'q5o4'},
-      {id: 6, question: 'fff', option1: 'q6o1', option2: 'q6o2', option3: 'q6o3', option4: 'q6o4'},
-      {id: 7, question: 'ggg', option1: 'q7o1', option2: 'q7o2', option3: 'q7o3', option4: 'q7o4'}];
-
-
+      {id: 1, question: 'aaa', option1: 'q1o1', option2: 'q1o2', option3: 'q1o3', option4: 'q1o4', answer: 'a'},
+      {id: 2, question: 'bbb', option1: 'q2o1', option2: 'q2o2', option3: 'q2o3', option4: 'q2o4', answer: 'b'},
+      {id: 3, question: 'ccc', option1: 'q3o1', option2: 'q3o2', option3: 'q3o3', option4: 'q3o4', answer: 'd'},
+      {id: 4, question: 'ddd', option1: 'q4o1', option2: 'q4o2', option3: 'q4o3', option4: 'q4o4', answer: 'c'},
+      {id: 5, question: 'eee', option1: 'q5o1', option2: 'q5o2', option3: 'q5o3', option4: 'q5o4', answer: 'r'},
+      {id: 6, question: 'fff', option1: 'q6o1', option2: 'q6o2', option3: 'q6o3', option4: 'q6o4', answer: 'a'},
+      {id: 7, question: 'ggg', option1: 'q7o1', option2: 'q7o2', option3: 'q7o3', option4: 'q7o4', answer: 'b'}];
 
   constructor(
       private getQuestionService: GetQuestionService
@@ -34,25 +38,49 @@ export class EvaluationComponent implements OnInit {
       this.getQuestionService.getQuestions().subscribe(data => this.questions = data);
       console.log(this.questions2[0].option1);
 
+      this.question = this.questions2[this.index].question;
       this.op1 = this.questions2[this.index].option1;
-      this.op2 = this.questions2[this.index2].option1;
+      this.op2 = this.questions2[this.index].option2;
+      this.op3 = this.questions2[this.index].option3;
+      this.op4 = this.questions2[this.index].option4;
 
-      this.index2$ = new BehaviorSubject(this.index2);
-      this.index2$.subscribe((currentValue) => { this.index2 = currentValue; });
+      this.index$ = new BehaviorSubject(this.index);
+      this.index$.subscribe((currentValue) => { this.index = currentValue; });
 
+      this.questionsLength = this.questions2.length;
+  }
+
+  updateQuestion(val: number) {
+      this.question = this.questions2[val].question,
+      this.op1 = this.questions2[val].option1;
+      this.op2 = this.questions2[val].option2;
+      this.op3 = this.questions2[val].option3;
+      this.op4 = this.questions2[val].option4;
   }
 
   onPreviousClick() {
-      this.index2 ++;
-      this.op2 = this.questions2[this.index2].option1;
-      this.index2$.next(this.index2);
-      console.log(this.op2);
+      this.index --;
+      this.updateQuestion(this.index);
+      this.index$.next(this.index);
+      this.disableButtonNext = false;
+      if (this.index <= 0) {
+          this.disableButtonPrevious = true;
+      }
   }
 
   onNextClick() {
-      this.index2 ++;
-      this.op2 = this.questions2[this.index2].option1;
-      this.index2$.next(this.index2);
+      this.index ++;
+      this.updateQuestion(this.index);
+      this.index$.next(this.index);
+      this.disableButtonPrevious = false;
+      if (this.index >= (this.questionsLength - 1)) {
+          this.disableButtonNext = true;
+      }
+
+  }
+
+  OnSelect(answer: string) {
+      alert("asdqwdq");
   }
 
 }
